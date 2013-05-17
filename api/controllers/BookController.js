39dda43f124033c503b7b45377b0f0058884f347
kevinob11 <kevinob11@gmail.com>
2013-05-17 14:29:40 -0700

    Added server side controllers for checking in and out a book so that doesn't have to be done by edit, this is so we can lock down the edit function but still allow anonymous users to checkout books.

--- api/controllers/BookController.js
+++ api/controllers/BookController.js
@@ -4,5 +4,38 @@
 ---------------------*/
 var BookController = {
 
+	/*---------------------
+		We create checkout and checkin functions because we don't want users
+		to have to be logged in to use them, but we don't want logged out users
+		to be able to update any field in a book. So we turn on auth for the
+		update function then build these wrappers for checkin and checkout.
+		We pass the entire user to these functions, but we only update the
+		checkout field to prevent people using these functions to update users.
+	---------------------*/
+
+	checkout: function (req, res) {
+
+		Book.update({id: req.body.id}, {checkout: req.body.checkout}, function(err,book){
+			if (err) {
+				res.send("Error checking out book.");
+			} else {
+				res.send("Book checked out successfully.");
+			}
+		});
+
+	},
+
+	checkin: function (req, res) {
+
+		Book.update({id: req.body.id}, {checkout: req.body.checkout}, function(err,book){
+			if (err) {
+				res.send("Error checking in book.");
+			} else {
+				res.send("Book checked in successfully.");
+			}
+		});
+
+	}
+
 };
 module.exports = BookController;
